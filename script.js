6c6dd15becf5e1407bd79535b696cf68f2f663a6
FST <faust.reloader@gmail.com>
2024-02-22 22:02:09 +0200

    php

---
+++ script.js
@@ -0,0 +1,13 @@
+// // Встановити змінну у локальному сховищі
+
+// function ggg() {
+//     let a = document.getElementById("ggg").className;
+//     alert(a);
+// }
+
+
+// localStorage.setItem('key', 'value');
+// // Отримати змінну з локального сховища
+// const myVariable = localStorage.getItem('key');
+// // Видалити змінну з локального сховища
+// localStorage.removeItem('key');
